94a0760c3c6f812f8dce4e684a882792e260b6dd
amnhmb <thirtyonelab.catalog@gmail.com>
2026-07-30 17:19:25 +0800

    Updates

--- assets/index-DD2apMEU.js
+++ assets/index-DD2apMEU.js
@@ -529,8 +529,6 @@ Resources:`;for(let t of c){if(!t||typeof t!=`string`)throw Error(`@supabase/aut
           display: flex;
           flex-direction: column;
           gap: 2.5rem;
-          max-height: 70vh;
-          overflow-y: auto;
         }
 
         .meta-section-title {
